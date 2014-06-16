bb3a22c07e44a82d199240a29347fd79bc1bf603
clicktx <clicktx@gmail.com>
2014-06-16 17:50:51 +0900

    Change プラグイン情報更新

--- plugin_info.php
+++ plugin_info.php
@@ -14,14 +14,14 @@
  */
 class plugin_info{
     static $PLUGIN_CODE       = "AjaxZip3";
-    static $PLUGIN_NAME       = "ajaxzip3連携 for EC-CUBE 2.13";
+    static $PLUGIN_NAME       = "ajaxzip3連携 for EC-CUBE 2.13 EC-CUBE on BootStrap3版";
     static $CLASS_NAME        = "AjaxZip3";
     static $PLUGIN_VERSION    = "2.1";
     static $COMPLIANT_VERSION = "2.13.0 - 2.13.1";
-    static $AUTHOR            = "株式会社システムフレンド";
-    static $DESCRIPTION       = "メンテナンスフリーの郵便番号検索API「ajaxzip3」を利用するようにします。郵便番号辞書のアップデートを行なわなくても良くなります。";
-    static $PLUGIN_SITE_URL   = "http://ec-cube.systemfriend.co.jp/";
-    static $AUTHOR_SITE_URL   = "http://www.systemfriend.co.jp/";
+    static $AUTHOR            = "clicktx";
+    static $DESCRIPTION       = "メンテナンスフリーの郵便番号検索API「ajaxzip3」を利用するようにします。郵便番号辞書のアップデートを行なわなくても良くなります。テンプレート「EC-CUBE on BootStrap3」に最適化済。";
+    static $PLUGIN_SITE_URL   = "https://github.com/clicktx/eccube-on-bootstrap3-plugin-AjaxZip3-for-2.13";
+    static $AUTHOR_SITE_URL   = "http://perl.no-tubo.net";
     static $HOOK_POINTS       = "prefilterTransform";
     static $LICENSE           = "MIT";
 }
